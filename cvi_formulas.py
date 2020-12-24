b581eb2c2b32e3ee42d57768e2030950719c4e6d
GAujla <74597216+GAujla@users.noreply.github.com>
2020-12-24 13:30:20 +0000

    Update cvi_formulas.py

--- cvi_formulas.py
+++ cvi_formulas.py
@@ -71,3 +71,11 @@ def pix_to_cam(x,y,z,a,b,ox,oy):
   print([u,v])
   return [u,v]
 
+import numpy as np
+
+def rgb2gray(rgb):
+
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray
